963cc8930f1ececf7d6997955b0ca4e80fff99d2
Janek <j.p.walaszek@gmail.com>
2022-12-06 21:11:31 +0100

    Add js script with credits

---
+++ main.js
@@ -0,0 +1,3 @@
+function creditsList () {
+    alert ("Photos by Pixabay, Lukas, Imprensa AgruBan®, Elle Hughes from: Pexels.com \nFont by Black Ops One designed by James Grieshaber & Eben Sorkin \nIcons by Eucalyp, max.icons, Freepik, Good Ware from: Flaticon \nBear design by dglm-studio from: Vecteezy")
+}
