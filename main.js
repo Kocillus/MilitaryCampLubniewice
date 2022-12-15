babdc9567460fe50ad072bd2e2a28ed953ea9a1c
Janek <j.p.walaszek@gmail.com>
2022-12-15 17:27:52 +0100

    modify credits

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function creditsList () {
-    alert ("Photos by Pixabay, Lukas, Imprensa AgruBan®, Elle Hughes from: Pexels.com \nFont by Black Ops One designed by James Grieshaber & Eben Sorkin \nIcons by Eucalyp, max.icons, Freepik, Good Ware from: Flaticon \nBear design by dglm-studio from: Vecteezy")
+    alert ("Photos by Pixabay, Lukas, Imprensa AgruBan®, Elle Hughes, Specna Arms from: Pexels.com \n\nFont by Black Ops One designed by James Grieshaber & Eben Sorkin \nIcons by Eucalyp, max.icons, Freepik, Good Ware from: Flaticon \n\nBear design by dglm-studio from: Vecteezy")
 }
